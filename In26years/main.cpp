51a53facc4697fef3dd36dac5d4f502b21d2af37
ephe <ephe@ephedeMacBook-Pro.local>
2020-03-16 22:09:02 +0800

    Initial Commit

---
+++ In26years/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  In26years
+//
+//  Created by ephe on 2020/3/16.
+//  Copyright © 2020年 xiaoqiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
