e10759b42a0cf3794b034e9b446333c090aebbde
ephe <ephe@ephedeMacBook-Pro.local>
2020-04-04 21:20:26 +0800

    Initial Commit

---
+++ MOOC_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MOOC_C++
+//
+//  Created by ephe on 2020/4/4.
+//  Copyright © 2020年 xiaoqiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
